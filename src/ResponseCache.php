4a2f6fc62a8e77cb56fc389b50ebfa3595e87855
Zamrony P. Juhara <zamronypj@yahoo.com>
2018-04-10 12:36:47 +0800

    fix namespace

--- src/ResponseCache.php
+++ src/ResponseCache.php
@@ -1,8 +1,8 @@
 <?php
 namespace Juhara\CacheMiddleware;
 
-use Psr\Http\ServerRequestInterface as Request;
-use Psr\Http\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
 use Juhara\ZzzCache\Contracts\Cacheable;
 
 /**
